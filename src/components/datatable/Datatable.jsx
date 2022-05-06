3f79d20519a9eb960334ea47ec1a924aadf26c14
Bio <amasin76@gmail.com>
2022-05-06 06:23:42 +0000

    🎆 initial

---
+++ src/components/datatable/Datatable.jsx
@@ -0,0 +1,53 @@
+import "./datatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { userColumns, userRows } from "../../services/datatablesource";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import { Link } from "react-router-dom";
+
+const Datatable = ({ title }) => {
+  const actionColumns = {
+    field: "action",
+    headerName: "Action",
+    width: 160,
+    renderCell: () => {
+      return (
+        <div className="cellAction">
+          <Link to="/users/1" style={{ textDecoration: "none" }}>
+            <div className="viewButton">
+              <VisibilityIcon className="icon" /> View
+            </div>
+          </Link>
+          <div className="deleteButton">
+            <DeleteIcon className="icon" /> Delete
+          </div>
+        </div>
+      );
+    },
+  };
+
+  return (
+    <div className="datatable">
+      <div className="top">
+        <h2 className="title">{title ? title : "Add New User"}</h2>
+        <div className="newButton">
+          <AddIcon className="icon" />
+          <span>NEW</span>
+        </div>
+      </div>
+      <div className="tableContainer">
+        <DataGrid
+          rows={userRows}
+          columns={userColumns.concat(actionColumns)}
+          pageSize={6}
+          rowsPerPageOptions={[6]}
+          checkboxSelection
+          autoHeight={true}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Datatable;
